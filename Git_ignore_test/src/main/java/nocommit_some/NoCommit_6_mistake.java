ee3a3a36507d98d9fb0ab1174d1b2527bf15f967
이구름 <@DESKTOP-OTMP5NS>
2023-10-25 11:39:07 +0900

    == NoCommit_6_right.java 파일입니다. ==

--- Git_ignore_test/src/main/java/nocommit_some/NoCommit_6_mistake.java
+++ Git_ignore_test/src/main/java/nocommit_some/NoCommit_6_mistake.java
@@ -4,7 +4,7 @@ public class NoCommit_6_mistake {
 
 	public static void main(String[] args) {
 		
-		System.out.println("== NoCommit_5_right.java 파일입니다 ==" );
+		System.out.println("== NoCommit_6_right.java 파일입니다. ==" );
 
 	}
 
